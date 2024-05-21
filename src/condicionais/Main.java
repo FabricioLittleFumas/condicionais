fef3c7f4c4b08f0466393d4c973f3e5cea39f9f6
FabricioGit2024 LittleFumas <fabriciogit2024@hotmail.com>
2024-05-21 06:33:10 -0300

    Teste de condicionais

---
+++ src/condicionais/Main.java
@@ -0,0 +1,13 @@
+package condicionais;
+
+public class Main {
+	public static void main(String[] args) {
+
+		int x = 40;
+		int y = 20;
+
+		if (x > y) {
+			System.out.println("x -> " + x + " é maior que y: " + y);
+		}
+	}
+}
